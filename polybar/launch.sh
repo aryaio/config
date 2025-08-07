45fccf3faf8cc4e525310164be67f0457de123b9
aryaio <aryaghayedi@gmail.com>
2025-08-07 23:42:46 +0330

    Updated launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,35 +1,14 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-#killall -q polybar
-
-# Wait until the processes have been shut down
-#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# If all your bars have ipc enabled, you can use 
+#polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+killall -q polybar
 
 # Launch bar1 and bar2
-#if [ "$1" == "light" ]
-#then
-#	polybar -c $HOME/.config/polybar/bars/light-config nord-top &
-#	polybar -c $HOME/.config/polybar/bars/light-config nord-down &
-#else
-#	polybar -c $HOME/.config/polybar/bars/dark-config nord-top &
-#	polybar -c $HOME/.config/polybar/bars/dark-config nord-down &
-#fi
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar duplicate 2>&1 | tee -a /tmp/polybar2.log & disown
 
-#echo "Bars launched..."
-#
-#
-#
-#
-#
-#!/bin/bash
-
-# Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
-
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar --config=/etc/polybar/config/config.ini main 2>&1 | tee -a /tmp/polybar.log & disown
-
-echo "Polybar launched..."
+echo "Bars launched..."
